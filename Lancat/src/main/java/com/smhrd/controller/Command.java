4ce0a91ca7c45effbf0ec7c8999a3fae63e11d01
smhrd <smhrd@121.179.35.18>
2023-01-18 12:37:54 +0900

    지금까짓

---
+++ Lancat/src/main/java/com/smhrd/controller/Command.java
@@ -0,0 +1,13 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Command {
+
+	  public String execute(HttpServletRequest request, HttpServletResponse response)
+	         throws ServletException, IOException;
+}
---
+++ Lancat/src/main/java/com/smhrd/frontcontroller/FrontController.java
@@ -0,0 +1,35 @@
+package com.smhrd.frontcontroller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
